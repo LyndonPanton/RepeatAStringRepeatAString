929a3032bf4395c3a7648a2e5b906fc13c260957
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-26 18:09:38 +0100

    Added logic for repeating a string

--- js/app.js
+++ js/app.js
@@ -3,8 +3,40 @@
 window.onload = function(event) {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
 
-	function repeat(string, number) {
+	function display(response) {
 		
+	}
+
+	function repeat(string, number) {
+		let repeat = "";
+
+		if (isNaN(number)) {
+			return "Enter a number";
+		} else if (number % 1 !== 0) {
+			return "Enter integeral value";
+		} else if (number < 1) {
+			return "Enter a number greater than 1";
+		} else {
+			// Method I
+			for (let i = 1; i <= number; i++) {
+				repeat = repeat + string;
+			}
+			
+			display(repeat);
+
+
+			// Method II
+			// repeat = [];
+
+			// for (let i = 1; i <= number; i++) {
+			// 	repeat.push(string);
+			// }
+
+			// display(repeat.join(""));
+
+			// Methdod III
+			// display((number > 0) ? string.repeat(number) : "");
+		}
 	}
 
 	let form = document.getElementById("form");
