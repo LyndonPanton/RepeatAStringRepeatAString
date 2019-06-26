27b7a0176e9d26d616394d96f7b58db5a7a21031
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-26 17:53:39 +0100

    Prevent default page refresh on form submission

--- js/app.js
+++ js/app.js
@@ -2,4 +2,15 @@
 
 window.onload = function(event) {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
+
+	function repeat(string, number) {
+		
+	}
+
+	let form = document.getElementById("form");
+	form.addEventListener("submit", function(event) {
+		event.preventDefault();
+
+		repeat(this.children[0].value, this.children[1].value);
+	});
 };
